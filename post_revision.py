ebfad64bfdefc167106078e9a89be67ecb36a72a
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-11-08 18:05:37 +0100

    Added *.pyc to gitignore, lazy history

--- post_revision.py
+++ post_revision.py
@@ -14,49 +14,65 @@ from collections import namedtuple
 
 from pelican import signals
 
-Revision = namedtuple('Revision', ['author', 'date', 'msg'], verbose=True)
 
-def generate_post_revision(generator):
-  project_root = generator.settings.get('PROJECT_ROOT', None)
-  github_url = generator.settings.get('GITHUB_URL', None)
-  github_branch = generator.settings.get('GITHUB_BRANCH', 'master')
-  if github_url is not None and github_url.endswith('/'):
-    github_url = github_url.rstrip('/')
+Revision = namedtuple('Revision', ['author', 'date', 'msg'])
 
-  pages = []
-  pages += getattr(generator, 'articles', [])
-  pages += getattr(generator, 'pages', [])
-  for page in pages:
-    path = getattr(page, 'source_path', None)
-    if path is None:
-      continue
-    try:
-      output = subprocess.check_output('git log --format="%%an|%%ai|%%s" %s'\
-          % (path), shell=True)
-      commits = []
-      for line in output.split('\n'):
+
+def lazy_evaluate(fn, *args, **kwargs):
+    """Decorator that makes a function lazy-evaluated."""
+    def _lazy_evaluate():
+        return fn(*args, **kwargs)
+    return _lazy_evaluate
+
+
+def generate_history(path):
+
+    output = subprocess.check_output('git log --format="%%an|%%ai|%%s" %s' % (path), shell=True)
+    commits = []
+    for line in output.split('\n'):
         line = line.strip()
         if len(line) == 0:
-          continue
+            continue
         parts = line.split('|')
         author, date, msg = parts[0], dateutil.parser.parse(parts[1]), '|'.join(parts[2:])
         commits.append(Revision(author, date, msg))
-      setattr(page, 'history', commits)
+        print "History evaluated"
+    return commits
 
-      if github_url is None or project_root is None:
-        continue
 
-      relative_path = path.replace(project_root, '')
-      if relative_path.startswith('/'):
-        relative_path = relative_path.lstrip('/')
+def generate_post_revision(generator):
 
-      setattr(page, 'github_history_url', '{gh_url}/commits/{branch}/{path}'.format(gh_url=github_url,
-                                                                                    branch=github_branch,
-                                                                                    path=relative_path))
-    except:
-      continue
+    project_root = generator.settings.get('PROJECT_ROOT', None)
+    github_url = generator.settings.get('GITHUB_URL', None)
+    github_branch = generator.settings.get('GITHUB_BRANCH', 'master')
+    if github_url is not None and github_url.endswith('/'):
+        github_url = github_url.rstrip('/')
+
+    pages = []
+    pages += getattr(generator, 'articles', [])
+    pages += getattr(generator, 'pages', [])
+    for page in pages:
+        path = getattr(page, 'source_path', None)
+        if path is None:
+            continue
+        try:
+            setattr(page, 'history', lazy_evaluate(generate_history, path=path))
+
+            if github_url is None or project_root is None:
+                continue
+
+            relative_path = path.replace(project_root, '')
+            if relative_path.startswith('/'):
+                relative_path = relative_path.lstrip('/')
+            setattr(page, 'relative_path', relative_path)
+            gh_url = '{gh_url}/commits/{branch}/{path}'.format(gh_url=github_url,
+                                                               branch=github_branch,
+                                                               path=relative_path)
+            setattr(page, 'github_history_url', gh_url)
+        except:
+            continue
 
 
 def register():
-  signals.article_generator_finalized.connect(generate_post_revision)
-  signals.page_generator_finalized.connect(generate_post_revision)
+    signals.article_generator_finalized.connect(generate_post_revision)
+    signals.page_generator_finalized.connect(generate_post_revision)
